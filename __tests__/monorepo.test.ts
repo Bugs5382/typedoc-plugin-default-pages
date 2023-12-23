ed34a84cdfbf0b113f0507a4be05f42942bf5163
“Bugs5382” <bugs5382@users.noreply.github.com>
2023-12-23 12:56:25 -0500

    feat: unit testing
    * added structure into the code
    * build CJS scripts from ts
    
    [ci skip]

---
+++ __tests__/monorepo.test.ts
@@ -0,0 +1,14 @@
+
+describe('typedoc plugin default pages - monorepo ', () => {
+
+  describe('pages', () => {
+    test.todo('...to write')
+  })
+  describe('documentation', () => {
+    test.todo('...to write')
+  })
+  describe('search index', () => {
+    test.todo('...to write')
+  })
+
+})
---
+++ __tests__/simple.test.ts
@@ -0,0 +1,13 @@
+describe('typedoc plugin default pages - single repo ', () => {
+
+  describe('pages', () => {
+    test.todo('...to write')
+  })
+  describe('documentation', () => {
+    test.todo('...to write')
+  })
+  describe('search index', () => {
+    test.todo('...to write')
+  })
+
+})
